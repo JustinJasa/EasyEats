4295c6aa733a1efa0dd944ed3727a5798b73d685
Justin Jasa <jasa@Justins-MacBook-Pro-2.local>
2023-03-03 12:49:21 +0100

    mobile responsiveness

--- components/mainbar.js
+++ components/mainbar.js
@@ -7,24 +7,24 @@ function Mainbar() {
   const [user, setUser] = useState(false);
 
   return (
-    <div className="flex flex-row justify-end p-6">
+    <div className="flex flex-row justify-end md:p-6 p-4">
       <input
         type="text"
         placeholder="Search Here!"
-        className="w-72 shadow-lg drop-shadow-md rounded-lg mr-12 p-3"
+        className="md:w-72 w-48 shadow-lg drop-shadow-md rounded-lg md:mr-12 m-8 p-3"
       />
       <div className="flex justify-center items-center">
         <div className="flex bg-black justify-center items-center mr-4 rounded-lg p-2 text-white">
-          <PlusIcon className="h-4 w-4 mr-2"/>
-          <p>Create Receipe</p>
+          <PlusIcon className="h-8 w-8 sm:h-4 sm:w-4 mr-2"/>
+          <p className="hidden sm:block">Create Receipe</p>
         </div>
         {user ? (
           <button>User</button>
         ) : (
           <Link href={`/login`}>
           <div className="flex items-center bg-green-600 text-white p-2 rounded-lg">
-            <UserIcon className="h-4 w-4 mr-2"/>
-            <button>Sign In</button>
+            <UserIcon className="h-8 w-8 sm:h-4 sm:w-4 mr-2"/>
+            <button className="hidden sm:block">Sign In</button>
           </div>
           </Link>
         )}
