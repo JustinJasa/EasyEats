075f0330e0d02c96c3201ee468fc7c4cf477d5a6
Justin Jasa <jasa@Justins-MacBook-Pro-2.local>
2023-03-05 14:03:06 +0100

    adding ingredients functionality

--- components/createrecipe.js
+++ components/createrecipe.js
@@ -1,11 +1,8 @@
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import { ArrowUpOnSquareIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { PlusIcon } from "@heroicons/react/24/outline";
-import { Editor, EditorState } from "draft-js";
-
 import Spinner from "./spinner";
-import InLineStyleControls from "./inlinestylecontrols";
 
 function CreateRecipe() {
   const [title, setTitle] = useState("");
@@ -17,14 +14,16 @@ function CreateRecipe() {
   const [imageAsset, setImageAsset] = useState();
   const [wrongImageType, setWrongImageType] = useState(false);
   const [user, setUser] = useState(false);
-  const [modal, setModal] = useState(false);
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  const [ingredientModal, setIngredientModal] = useState(false);
+  const [instructionModal, setInstructionModal] = useState(false);
+  const [ingredients, setIngredients] = useState([])
+  const [newIngredient, setNewIngredient] = useState([])
 
-  console.log(editorState)
+  console.log(ingredients)
 
-  let ingredients = [];
+  let images = [];
+
+  let instructions = [];
 
   let categories = [
     "Recommended",
@@ -48,6 +47,7 @@ function CreateRecipe() {
     "Non-Spicy",
   ];
 
+
   const savePin = () => {
     console.log("hi");
   };
@@ -55,6 +55,17 @@ function CreateRecipe() {
   const uploadImage = () => {
     console.log("hi");
   };
+
+  const submitIngredient = (e) => {
+    e.preventDefault();
+    if (!newIngredient) return;
+    setIngredients([...ingredients, { id: Date.now(), text: newIngredient }]);
+    setNewIngredient('');
+  }
+
+  const handleTodoDelete = (id) => { 
+    setIngredients(ingredients.filter((ingredients) => ingredients.id !== id));
+  }
 
   return (
     <div className="flex flex-col justify-center items-center lg:ml-20">
@@ -65,18 +76,17 @@ function CreateRecipe() {
       )}
       <div className=" flex flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-3/5 w-full mt-12">
         <h2 className="text-4xl font-bold mb-4">Create a Recipe 🥘 </h2>
+        <h4 className="text-lg italic mb-4">Share your wonderful recipe to world!</h4>
         <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
           <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
             {loading && <Spinner />}
             {wrongImageType && <p>It&apos;s wrong file type.</p>}
             {!imageAsset ? (
               // eslint-disable-next-line jsx-a11y/label-has-associated-control
-              <label>
-                <div className="flex flex-col items-center justify-center h-full">
+              <label className="cursor-pointer">
+                <div className="flex flex-col items-center justify-center h-full ">
                   <div className="flex flex-col justify-center items-center">
-                    <p className="font-bold text-2xl">
-                      <ArrowUpOnSquareIcon className="text-black" />
-                    </p>
+                    <ArrowUpOnSquareIcon className="text-black h-8 w-8" />
                     <p className="text-lg">Click to upload</p>
                   </div>
 
@@ -139,15 +149,15 @@ function CreateRecipe() {
               }}
               className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
             >
-              <option value="others" className="sm:text-bg bg-white">
+              <option value="others" className="sm:text-bg bg-white p-2">
                 Select Category
               </option>
               {categories.map((item) => (
                 <option
-                  className="text-base border-0 outline-none capitalize bg-white text-white"
-                  value={item.name}
+                  className="text-base border-0 outline-none text-black"
+                  value={item}
                 >
-                  {item.name}
+                  {item}
                 </option>
               ))}
             </select>
@@ -157,37 +167,40 @@ function CreateRecipe() {
             <div className="flex justify-between pr-4 items-center">
               <h2 className="font-bold text-2xl">Ingredients</h2>
               <PlusIcon
-                className="h-8 w-8 lg:h-8 lg:w-8 mr-2 bg-black text-white rounded-lg"
-                onClick={() => setModal(!modal)}
+                className="h-8 w-8 lg:h-6 lg:w-6 mr-2 bg-black text-white rounded-lg"
+                onClick={() => setIngredientModal(!ingredientModal)}
               />
             </div>
-            {modal && (
-              <div class="fixed z-10 top-0 left-0 w-full h-full bg-black bg-opacity-40 modal">
-                <div class="bg-white w-80 md:w-auto mx-auto mt-32 md:mt-0 rounded-md border border-gray-300 modal-content">
+            {ingredientModal && (
+              <div class="fixed z-5 top-0 left-0 w-full h-full bg-black bg-opacity-40">
+                <div class="bg-white w-2/5 mx-auto mt-32 rounded-md border border-gray-300">
                   <span
-                    class="text-gray-500 text-right text-2xl block cursor-pointer"
-                    onClick={() => setModal(!modal)}
+                    class="text-gray-500 text-right text-2xl block cursor-pointer p-4"
+                    onClick={() => setIngredientModal(!ingredientModal)}
                   >
                     ×
                   </span>
-                  <div class="p-4">
-                    <p>Some text in the Modal..</p>
+                  <div class="p-2 flex flex-col items-center justify-center">
+                    <form onSubmit={(e) => submitIngredient(e)}>
+                    <input className="w-full mb-4 p-2" type="text" placeholder="Type ingredient and quantity here!" value={newIngredient} onChange={(e) => setNewIngredient(e.target.value)}/>
+                    <button className="bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none" type="submit">Submit</button>
+                    </form>
                   </div>
                 </div>
               </div>
             )}
-            <div></div>
+            <div>
+              {ingredients && ingredients.map((item) => (
+                  <li className="cursor-pointer" key={item.id} onClick={() => handleTodoDelete(ingredients.id)}>
+                    {item.text}
+                  </li>
+                )
+              )}
+            </div>
           </div>
 
           <div>
             <h2 className="font-bold text-2xl mb-2">Instructions</h2>
-            <InLineStyleControls editorState={editorState}/>
-            <Editor
-              className="border-solid border-2"
-              editorState={editorState}
-              onChange={setEditorState}
-              placeholder="List instructions here!"
-            />
           </div>
           <div className="flex flex-col">
             <div className="flex justify-end items-end mt-5">
@@ -196,7 +209,7 @@ function CreateRecipe() {
                 onClick={savePin}
                 className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
               >
-                Save Pin
+                Save Recipe
               </button>
             </div>
           </div>
