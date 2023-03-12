d2dd81adb6b5b2102255d5bc16d7b97f41528235
Justin Jasa <jasa@Justins-MacBook-Pro-2.local>
2023-03-12 01:03:51 +0100

    adding links

--- components/landing.js
+++ components/landing.js
@@ -5,24 +5,28 @@ function Landing() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div>
-        {/* <img className="w-32 h-32" src="https://img.icons8.com/ios/512/man-logo.png" alt="Logo" /> */}
+        <img className="w-32 h-32" src="https://img.icons8.com/ios/512/man-logo.png" alt="Logo" />
       </div>
       <h1 className="font-serif lg:text-6xl">
-        Food for Students... By Students.
+        Food for students... by students.
       </h1>
       <h3 className="text-slate-500 lg:text-lg m-4 italic">
-        Freshly curated recipes created by students around the world.
+        Freshly curated recipes by students around the world.
       </h3>
-      <div className="flex w-4/5">
+      <div className="flex items-center space-between">
         <Link href={`/login`}>
-          <button className="m-4 text-lg text-white bg-green-600 w-full rounded-md lg:hover:scale-110  transition transition-duration: 150ms">
-            Sign Up
-          </button>
+          <div className="mr-8">
+            <button className="text-lg text-white bg-green-600 border-2 border-green-600 rounded-md lg:hover:scale-110 pl-20 pr-20 pt-2 pb-2 transition transition-duration: 150ms">
+              Sign Up
+            </button>
+          </div>
         </Link>
         <Link href={`/signup`}>
-          <button className="m-4 text-lg text-green-600 border-solid border-2 border-green-600 w-full p-2 rounded-md transition transition-duration: 150ms lg:hover:scale-110">
-            Sign In
-          </button>
+          <div>
+            <button className="text-lg text-green-600 border-solid border-2 border-green-600  pl-20 pr-20 pt-2 pb-2  rounded-md transition transition-duration: 150ms lg:hover:scale-110">
+              Sign In
+            </button>
+          </div>
         </Link>
       </div>
     </div>
