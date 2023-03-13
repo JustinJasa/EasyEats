d96b434690b0594058fe2ea9b34267a99facc209
Justin Jasa <jasa@Justins-MacBook-Pro-2.local>
2023-03-13 15:55:58 +0100

    getting rid of transtion import

--- components/comments.js
+++ components/comments.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import CommentComponent from "./comment";
 
