3336b67ef8e599ecc4aaf1478192010b4c67f629
unknown <memogarcia246@gmail.com>
2023-03-22 14:21:21 +0100

    some information of the pin is being displayed

--- components/pin.js
+++ components/pin.js
@@ -1,9 +1,45 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function Pin({ pin, index }) {
+function Pin({ pin, index, session }) {
+  const [imageURL, setImageURL] = useState(null)
   const [height, setHeight] = useState();
-
   const heights = [24, 48, 32, 80];
+
+  const token = session.user.token;
+  const userId = session.user.account[0].user_id;
+
+  const getRecipeImage = async (recipeId, imageId) => {
+    try {
+      if(imageId === null) {
+        throw "Image is null"
+      }
+      console.log(`Im recipe ${recipeId} trying to access image ${imageId}`)
+
+      const response = await axios.get(`http://localhost:8000/recipes/${recipeId}/images/${imageId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+
+      // Something needs to be fixed here so the image is displayed:
+      // - - - - - - - - - -
+      const blob = response.data
+      const url = URL.createObjectURL(blob)
+      // - - - - - - - - - -
+      setImageURL(url)
+    }
+    catch(error) {
+      switch(error) {
+        case "Image is null": {
+          console.log(error)
+          break
+        }
+        default: {
+          console.log(error)
+          break
+        }
+      }
+    }
+  }
 
   function getRandomHeight(heights) {
     // generate a random index between 0 and the length of the array minus 1
@@ -15,20 +51,26 @@ function Pin({ pin, index }) {
   useEffect(() => {
     const length = getRandomHeight(heights);
     setHeight(length);
+
+    getRecipeImage(index, pin.image_id)
   });
 
   return (
     <div key={index} className={`align-center content-center rounded-xl m-4`}>
-      <img
-        className={`rounded-2xl h-${height} w-full`}
-        src={pin.image}
-        alt="Main pin image"
-      ></img>
-      <h2 className="italic text-sm text-gray-500 mt-2">{pin.date}</h2>
-      <h2 className="font-bold font-serif text-xl mt-2">{pin.title}</h2>
+      {
+        imageURL && (
+          <img
+            className={`rounded-2xl h-${height} w-full`}
+            src={imageURL}
+            alt="Main pin image"
+          ></img>
+        )
+      }
+      <h2 className="italic text-sm text-gray-500 mt-2">{pin.date ? pin.date : "Sample date"}</h2>
+      <h2 className="font-bold font-serif text-xl mt-2">{pin.name}</h2>
       <div className="mt-2 flex">
-        <h3 className="font-bold mr-2">{pin.author}</h3>
-        <img className="rounded-lg" src={pin.profileImage} alt="Author Image" />
+        <h3 className="font-bold mr-2">{pin.username}</h3>
+        {/* <img className="rounded-lg" src={pin.profileImage} alt="Author Image" /> */}
       </div>
     </div>
   );
