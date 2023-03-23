1d8731555eb89f6ba7a34f8ca99e3350fe935f83
Justin Jasa <jasa@Justins-MacBook-Pro-2.local>
2023-03-24 00:07:18 +0100

    style changes

--- components/mainbar.js
+++ components/mainbar.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  UserIcon,
-  PlusIcon,
-} from "@heroicons/react/24/outline";
+import { UserIcon, PlusIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 function Mainbar({ session }) {
   const [query, setQuery] = useState("");
-  const router = useRouter()
-  
+  const router = useRouter();
+
   return (
     <div className="flex flex-row justify-end md:p-6 md:mt-6 md:mb-6">
       <div className="md:mr-12 flex justify-center items-center">
@@ -20,7 +17,10 @@ function Mainbar({ session }) {
             class="md:w-72 w-48 py-2 px-4 border-gray-300 rounded-l-lg focus:outline-none shadow-lg drop-shadow-md"
             onChange={(e) => setQuery(e.target.value)}
           />
-          <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-r-lg shadow-lg drop-shadow-md" onClick={() => router.push(`/main?search=${query}`)}>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-r-lg shadow-lg drop-shadow-md"
+            onClick={() => router.push(`/main?search=${query}`)}
+          >
             🔎 Search
           </button>
         </div>
@@ -34,10 +34,12 @@ function Mainbar({ session }) {
         </Link>
         {session && (
           <Link href={`/users/${session.user.account[0].username}`}>
-          <div className="flex bg-green-600 justify-center items-center mr-4 rounded-lg p-2 text-white hover:bg-green-800">
-            <UserIcon className="h-8 w-8 sm:h-4 sm:w-4 mr-2" />
-            <p className="hidden sm:block">{session.user.account[0].username}</p>
-          </div>
+            <div className="flex bg-green-600 justify-center items-center mr-4 rounded-lg p-2 text-white hover:bg-green-800">
+              <UserIcon className="h-8 w-8 sm:h-4 sm:w-4 mr-2" />
+              <p className="hidden sm:block">
+                {session.user.account[0].username}
+              </p>
+            </div>
           </Link>
         )}
       </div>
